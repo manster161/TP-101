9e27801d96aa8530a992ed1bffe1c7b3402b26ab
manster161 <manster161@gmail.com>
2016-11-06 22:11:55 +0100

    Continued work

--- src/tp101.cpp
+++ src/tp101.cpp
@@ -1,10 +1,20 @@
 #include "tp101.h"
 #include "pins.h"
-
+#include "config/secrets.h"
 Tp101::Tp101(){
   r1 = new Relay(RELAY1);
   r2 = new Relay(RELAY2);
   r3 = new Relay(RELAY3);
   r4 = new Relay(RELAY4);
   dht = new DHT(DHTPIN, DHTTYPE, 11);
+  server = new ESP8266WebServer(80);
+  wifiClient = new WiFiClient();
+  httpClient = new HTTPClient();
+}
+
+
+void Tp101::Init(){
+
+
+
 }
--- src/tp101.h
+++ src/tp101.h
@@ -1,19 +1,27 @@
 #ifndef _TP_H
 #define _TP_H
 #include "relay.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
 #include <DHT.h>
 
 class Tp101{
 public:
   Tp101(void);
   ~Tp101(void);
+  void Init();
 private:
   Relay* r1;
   Relay* r2;
   Relay* r3;
   Relay* r4;
-
   DHT* dht;
+  ESP8266WiFiMulti* WiFiMulti;
+  ESP8266WebServer* server;
+  HTTPClient* httpClient;
+  WiFiClient* wifiClient;
 };
 
 
